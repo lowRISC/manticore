2d56a595ba8a1f8f83b101e7adf3f824aafa79a6
Miguel Young de la Sota <mcyoung@google.com>
2020-06-19 14:36:49 -0400

    [protocol] Add `make_fuzz_safe!`.
    `make_fuzz_safe!` is a by-example macro that takes simple protocol
    structs with lifetimes and generates a `_: 'static` version that
    `Arbitrary` can be safely derived on.
    
    In the future, we may want to change this into a somewhat cleaner derive
    macro.
    
    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>

--- src/protocol/macros.rs
+++ src/protocol/macros.rs
@@ -50,3 +50,66 @@ macro_rules! wire_enum {
         }
     }
 }
+
+#[cfg(feature = "arbitrary-derive")]
+/// Convenience trait for use with `make_fuzz_safe`.
+#[doc(hidden)]
+pub trait FuzzSafe {
+    type Safe: libfuzzer_sys::arbitrary::Arbitrary;
+}
+
+/// Convenience macro for generating a "fuzz-safe" version of the struct that
+/// can be cheaply converted into the original struct.
+macro_rules! make_fuzz_safe {
+    (
+        $(#[$meta:meta])*
+        $vis:vis struct $name:ident $(<$lt:lifetime>)? as $wrapper_name:ident {$(
+            $(#[$field_meta:meta])*
+            $field_vis:vis $field:ident:
+                // Because we need to destructure on this, this *cannot* be a
+                // type. Hence, we accept a token-tree instead, with the
+                // understanding that callers will wrap complex types in
+                // parentheses.
+                //
+                // This is a limitation of the macro engine. See
+                // https://danielkeep.github.io/tlborm/book/mbe-min-captures-and-expansion-redux.html
+            $field_ty:tt,
+        )*}
+    ) => {
+        $(#[$meta])*
+        #[allow(unused_parens)]
+        $vis struct $name $(<$lt>)? {$(
+            $(#[$field_meta])*
+            $field_vis $field: $field_ty,
+        )*}
+
+        #[cfg(feature = "arbitrary-derive")]
+        #[derive(Clone, Debug, Arbitrary)]
+        #[allow(unused_parens)]
+        #[doc(hidden)]
+        $vis struct $wrapper_name {$(
+            $field: make_fuzz_safe!(@convert_ty, $field_ty),
+        )*}
+        #[cfg(feature = "arbitrary-derive")]
+        impl $wrapper_name {
+            /// Borrow this value into a protocol struct.
+            pub fn as_ref<$($lt)?>(&$($lt)? self)
+            -> $name<$($lt)?> {
+                $name {$(
+                    $field: make_fuzz_safe!(@extract_ty,
+                                            self.$field: $field_ty),
+                )*}
+            }
+        }
+
+        impl<$($lt)?> $crate::protocol::macros::FuzzSafe for $name<$($lt)?> {
+            type Safe = $wrapper_name;
+        }
+    };
+    (@convert_ty, (&$lt:tt [$ty:ty])) => {std::boxed::Box<[T]>};
+    (@convert_ty, (&$lt:tt str)) => {std::boxed::Box<str>};
+    (@convert_ty, (&$lt:tt $ty:ty)) => {$ty};
+    (@convert_ty, $ty:ty) => {$ty};
+    (@extract_ty, $s:tt.$f:tt: (&$lt:lifetime $ty:ty)) => {&$s.$f};
+    (@extract_ty, $s:tt.$f:tt: $ty:ty) => {$s.$f};
+}
