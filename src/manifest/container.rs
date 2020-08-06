815e9dcd27044dd384fa3a4f80c58f4d62fa9ab3
Miguel Young de la Sota <mcyoung@google.com>
2020-08-06 15:12:19 -0400

    [manifest] Add Containerizer, a manifest builder
    Containerizer is a type for building a manifest container with
    particular data and signing it, while writing it to a pre-existing
    buffer. This type will be useful for tools that need to actually create
    and sign such manifests.
    
    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>

--- src/manifest/container.rs
+++ src/manifest/container.rs
@@ -40,9 +40,13 @@
 //! are Manticore-specific.
 
 use crate::crypto::rsa;
+use crate::io;
+use crate::io::cursor::SeekPos;
+use crate::io::Cursor;
 use crate::io::Read as _;
-use crate::manifest::ManifestType;
+use crate::io::Write;
 use crate::manifest::Error;
+use crate::manifest::ManifestType;
 use crate::protocol::wire::WireEnum;
 
 /// Metadata for a [`Container`].
@@ -74,6 +78,12 @@ pub struct Container<'m> {
     metadata: Metadata,
     body: &'m [u8],
 }
+
+/// Offsets for fields within the container header.
+const LEN_OFFSET: usize = 0;
+const TYPE_OFFSET: usize = 2;
+const ID_OFFSET: usize = 4;
+const SIG_LEN_OFFSET: usize = 8;
 
 /// The length of the container header in bytes:
 /// two halves, a word, another half, and two bytes of padding.
@@ -116,14 +126,11 @@ impl<'m> Container<'m> {
         // sliced off in this operation).
         let rest = &buf[..len][HEADER_LEN..];
 
-        let body_len = rest
-            .len()
-            .checked_sub(sig_len)
-            .ok_or(Error::OutOfRange)?;
+        let body_len =
+            rest.len().checked_sub(sig_len).ok_or(Error::OutOfRange)?;
         let (body, sig) = rest.split_at(body_len);
 
-        let signed_len =
-            len.checked_sub(sig_len).ok_or(Error::OutOfRange)?;
+        let signed_len = len.checked_sub(sig_len).ok_or(Error::OutOfRange)?;
         let signed = &buf[..signed_len];
 
         rsa.verify_signature(sig, signed)
@@ -165,6 +172,157 @@ impl<'m> Container<'m> {
     pub fn body(&self) -> &'m [u8] {
         self.body
     }
+
+    /// Re-serializes this `Container` into its binary format.
+    ///
+    /// Re-serialization will not be exact; in particular, having the same
+    /// signature will depend on having the original signing keys.
+    ///
+    /// For more fine-grained control of serialiation, see [`Containerizer`].
+    ///
+    /// [`Containerizer`]: struct.Containerizer.html
+    pub fn containerize<'buf>(
+        &self,
+        signer: &mut impl rsa::Signer,
+        buf: &'buf mut [u8],
+    ) -> Result<&'buf mut [u8], Error> {
+        let mut builder = Containerizer::new(buf)?
+            .with_type(self.manifest_type())?
+            .with_metadata(self.metadata())?;
+        builder.write_bytes(self.body())?;
+        builder.sign(signer)
+    }
+}
+
+/// A [`Write`] implementation for writing new manifest containers.
+///
+/// Once all the parts of the container have been initialized, `sign()` can
+/// be used to produce an authentic, encoded manifest.
+///
+/// [`Write`]: ../../io/trait.Write.html
+pub struct Containerizer<'m> {
+    // Invariant: at all times, the pointer within the cursor should point
+    // beyond the "header" portion; `seek()` is used to move the cursor back
+    // when modifying the header.
+    cursor: Cursor<'m>,
+
+    has_type: bool,
+    has_metadata: bool,
+}
+
+impl<'m> Containerizer<'m> {
+    /// Creates a new, empty containerizer.
+    ///
+    /// This function does not perform I/O, but it will verify that `out`
+    /// is at least large enough to hold a [`Container`] header.
+    ///
+    /// Typically, this function will followed up by calls to other
+    /// `Containerizer` functions before it is used as a [`Write`].
+    ///
+    /// # Example
+    /// ```
+    /// # use manticore::manifest::{*, container::*};
+    /// # use manticore::io::Write as _;
+    /// # let mut buf = [0; 64];
+    /// let mut builder = Containerizer::new(&mut buf)?
+    ///     .with_type(ManifestType::Fpm)?
+    ///     .with_metadata(&Metadata { version_id: 42 })?;
+    /// builder.write_bytes(b"manifest contents stuff")?;
+    /// # Ok::<(), Error>(())
+    /// ```
+    ///
+    /// [`Container`]: struct.Container.html
+    /// [`Write`]: ../../io/trait.Write.html
+    pub fn new(out: &'m mut [u8]) -> Result<Self, Error> {
+        let mut cursor = Cursor::new(out);
+        cursor.seek(SeekPos::Abs(HEADER_LEN))?;
+
+        Ok(Self {
+            cursor,
+            has_type: false,
+            has_metadata: false,
+        })
+    }
+
+    /// Writes the given [`ManifestType`] into this `Containerizer`.
+    ///
+    /// [`ManifestType`]: ../enum.ManifestType.html
+    #[inline]
+    pub fn with_type(
+        mut self,
+        manifest_type: ManifestType,
+    ) -> Result<Self, Error> {
+        let mark = self.cursor.consumed_len();
+        self.cursor.seek(SeekPos::Abs(TYPE_OFFSET))?;
+        self.cursor.write_le(manifest_type.to_wire_value())?;
+        self.cursor.seek(SeekPos::Abs(mark))?;
+
+        self.has_type = true;
+        Ok(self)
+    }
+
+    /// Writes the given [`Metadata`] into this `Containerizer`.
+    ///
+    /// [`Metadata`]: struct.Metadata.html
+    #[inline]
+    pub fn with_metadata(mut self, metadata: &Metadata) -> Result<Self, Error> {
+        let mark = self.cursor.consumed_len();
+        self.cursor.seek(SeekPos::Abs(ID_OFFSET))?;
+        self.cursor.write_le(metadata.version_id)?;
+        self.cursor.seek(SeekPos::Abs(mark))?;
+
+        self.has_metadata = true;
+        Ok(self)
+    }
+
+    /// Completes the containerization process by signing all of the contents
+    /// and producing an encoded [`Container`].
+    ///
+    /// `with_type()` and `with_metadata()` must have been called before this
+    /// function is called; otherwise, an error will be returned.
+    ///
+    /// [`Container`]: struct.Container.html
+    pub fn sign(
+        mut self,
+        signer: &mut impl rsa::Signer,
+    ) -> Result<&'m mut [u8], Error> {
+        if !self.has_type || !self.has_metadata {
+            return Err(Error::OutOfRange);
+        }
+
+        let sig_len = signer.pub_len().byte_len();
+        let total_len = self
+            .cursor
+            .consumed_len()
+            .checked_add(sig_len)
+            .ok_or(io::Error::BufferExhausted)?;
+
+        let u16_max = u16::MAX as usize;
+        if total_len > u16_max || sig_len > u16_max {
+            return Err(Error::OutOfRange);
+        }
+
+        let mark = self.cursor.consumed_len();
+        self.cursor.seek(SeekPos::Abs(LEN_OFFSET))?;
+        self.cursor.write_le(total_len as u16)?;
+        self.cursor.seek(SeekPos::Abs(SIG_LEN_OFFSET))?;
+        self.cursor.write_le(sig_len as u16)?;
+        // Always set the "padding" bytes to 0xff.
+        self.cursor.write_le(0xffffu16)?;
+        self.cursor.seek(SeekPos::Abs(mark))?;
+
+        let (message, sig) = self.cursor.consume_with_prior(sig_len)?;
+        signer
+            .sign(message, sig)
+            .map_err(|_| Error::SignatureFailure)?;
+        Ok(self.cursor.take_consumed_bytes())
+    }
+}
+
+impl Write for Containerizer<'_> {
+    fn write_bytes(&mut self, buf: &[u8]) -> Result<(), io::Error> {
+        self.cursor.write_bytes(buf)
+    }
 }
 
 #[cfg(test)]
@@ -184,7 +342,7 @@ mod test {
         0x1f, 0x01, // Total length. This is the header length (12) +
         //          // body length (19) + signature length (256).
         0x0e, 0xda, // FPM magic.
-        0xaa, 0x55, 0x01, 0x00, // Container id (0x55aa).
+        0xaa, 0x55, 0x01, 0x00, // Container id (0x155aa).
         0x00, 0x01, // Signature length (0x100 = 256).
         0xff, 0xff, // Padding to 4 bytes.
     ];
@@ -260,5 +418,67 @@ mod test {
             Container::parse_and_verify(&manifest, &mut rsa),
             Err(Error::SignatureFailure)
         ));
+    }
+
+    #[test]
+    fn build_manifest() {
+        let keypair =
+            ring::rsa::Keypair::from_pkcs8(testdata::RSA_2048_PRIV_PKCS8)
+                .unwrap();
+        let pub_key = keypair.public();
+        let rsa_builder = ring::rsa::Builder::new();
+        let mut signer = rsa_builder.new_signer(keypair).unwrap();
+        let mut rsa = rsa_builder.new_engine(pub_key).unwrap();
+
+        let mut buf = vec![0; 1024];
+        let mut builder = Containerizer::new(&mut buf)
+            .unwrap()
+            .with_type(ManifestType::Fpm)
+            .unwrap()
+            .with_metadata(&Metadata {
+                version_id: 0x155aa,
+            })
+            .unwrap();
+        builder.write_bytes(MANIFEST_CONTENTS).unwrap();
+        let manifest_bytes = builder.sign(&mut signer).unwrap();
+        assert_eq!(
+            &manifest_bytes[..HEADER_LEN],
+            &MANIFEST_HEADER[..HEADER_LEN]
+        );
+
+        let manifest =
+            Container::parse_and_verify(manifest_bytes, &mut rsa).unwrap();
+        assert_eq!(manifest.manifest_type(), ManifestType::Fpm);
+        assert_eq!(manifest.metadata().version_id, 0x155aa);
+        assert_eq!(manifest.body(), MANIFEST_CONTENTS);
+    }
+
+    #[test]
+    fn roumd_trip() {
+        let keypair =
+            ring::rsa::Keypair::from_pkcs8(testdata::RSA_2048_PRIV_PKCS8)
+                .unwrap();
+        let pub_key = keypair.public();
+        let rsa_builder = ring::rsa::Builder::new();
+        let mut signer = rsa_builder.new_signer(keypair).unwrap();
+        let mut rsa = rsa_builder.new_engine(pub_key).unwrap();
+
+        let mut manifest = MANIFEST_HEADER.to_vec();
+        manifest.extend_from_slice(MANIFEST_CONTENTS);
+
+        let mut sig = vec![0; signer.pub_len().byte_len()];
+        signer.sign(&manifest, &mut sig).unwrap();
+
+        manifest.extend_from_slice(&sig);
+        assert_eq!(manifest.len(), MANIFEST_LEN);
+
+        let parsed_manifest =
+            Container::parse_and_verify(&manifest, &mut rsa).unwrap();
+
+        let mut buf = vec![0; 512];
+        let new_manifest_bytes =
+            parsed_manifest.containerize(&mut signer, &mut buf).unwrap();
+        // Note that this assumes that the padding bytes are always 0xffff.
+        assert_eq!(&manifest[..], new_manifest_bytes);
     }
 }
