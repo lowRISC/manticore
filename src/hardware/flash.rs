79d57375aac385bf8b24c401afa0cd0e933470ac
Miguel Young de la Sota <mcyoung@google.com>
2020-10-21 11:21:07 -0400

    [hardware] Add Flash::program() and FlashZero
    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>

--- src/hardware/flash.rs
+++ src/hardware/flash.rs
@@ -12,11 +12,42 @@
 //!
 //! [`Flash`]: trait.Flash.html
 
+use core::convert::TryInto;
+
+use static_assertions::assert_obj_safe;
+
 use zerocopy::AsBytes;
 use zerocopy::FromBytes;
 
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
+
+use crate::io;
+use crate::mem::Arena;
+
+/// A [`Flash`] error.
+///
+/// All of these errors are non-retryable; a [`Flash`] implementation should
+/// block until the operation succeeds.
+///
+/// [`Flash`]: trait.Flash.html
+#[derive(Copy, Clone, Debug)]
+pub enum Error {
+    /// Indicates that an operation failed because the requested
+    /// operation was outside of the device's address space.
+    OutOfRange,
+
+    /// Indicates that the device is locked in some manner and cannot
+    /// be affected by the operation.
+    Locked,
+
+    /// Indicates that an internal invariant was violated, such as running out
+    /// of memory.
+    Internal,
+
+    /// Indicates that an unspecified error occured.
+    Unspecified,
+}
 
 /// Provides access to a flash-like storage device.
 ///
@@ -27,21 +58,194 @@ use serde::{Deserialize, Serialize};
 ///
 /// The `Flash` trait comes implemented for `[u8]`.
 pub trait Flash {
-    /// The error type returned by transactions with this `Flash`.
-    type Error: Sized;
+    /// Returns the size, in bytes, of this device.
+    fn size(&self) -> Result<u32, Error>;
 
-    /// Gets the size, in bytes, of this device.
-    fn size(&self) -> Result<u32, Self::Error>;
+    /// Attempts to read `out.len()` bytes starting at `offset`.
+    fn read(&self, offset: FlashPtr, out: &mut [u8]) -> Result<(), Error>;
 
-    /// Attempt to read `slice` into `out`.
+    /// Attempts to write `out.len()` bytes starting at `offset`.
     ///
-    /// If `out` is smaller than `slice.len`, only the first `out.len()` bytes
-    /// will be read.
-    fn read(
-        &self,
-        slice: FlashSlice,
-        out: &mut [u8],
-    ) -> Result<(), Self::Error>;
+    /// Note that this function is not guaranteed to succeed (and be
+    /// reflected in the return value of `read`) until `flush()` is called.
+    /// This is to permit a `Flash` implementation to buffer writes before
+    /// sending them out.
+    ///
+    /// Implementations are, as an optimization, permitted to assume that
+    /// writes will be serial and localized, so as to minimize clearing
+    /// operations on flash hardware.
+    fn program(&mut self, offset: FlashPtr, buf: &[u8]) -> Result<(), Error>;
+
+    /// Flushes any pending `program()` operations.
+    fn flush(&mut self) -> Result<(), Error> {
+        Ok(())
+    }
+}
+assert_obj_safe!(Flash);
+
+impl<F: Flash> Flash for &mut F {
+    #[inline]
+    fn size(&self) -> Result<u32, Error> {
+        F::size(self)
+    }
+
+    #[inline]
+    fn read(&self, offset: FlashPtr, out: &mut [u8]) -> Result<(), Error> {
+        F::read(self, offset, out)
+    }
+
+    #[inline]
+    fn program(&mut self, offset: FlashPtr, buf: &[u8]) -> Result<(), Error> {
+        F::program(self, offset, buf)
+    }
+
+    #[inline]
+    fn flush(&mut self) -> Result<(), Error> {
+        F::flush(self)
+    }
+}
+
+/// A [`Flash`] type that allows for zero-copy reads.
+///
+/// This trait allows implementations that can support a zero-copy read,
+/// perhaps due to their inherently-buffering nature, to do so directly.
+///
+/// Normal [`Flash`] implementations can be made to support zero-copy
+/// reads by combining them with an [`Arena`]; see [`ArenaFlash`].
+///
+/// `[u8]` implements this trait with no overhead.
+///
+/// [`Flash`]: trait.Flash.html
+/// [`ArenaFlash]: struct.ArenaFlash.html
+pub trait FlashZero: Flash {
+    /// Attempts to zero-copy read the given region out of this device.
+    fn read_zerocopy(&self, slice: FlashSlice) -> Result<&[u8], Error>;
+
+    /// Hints to the implementation that it can release any buffered contents
+    /// it is currently holding.
+    ///
+    /// See [`Arena::reset()`].
+    ///
+    /// [`Arena::reset()`]: ../../mem/trait.Arena.html#tymethod.reset
+    fn reset(&mut self) -> Result<(), Error> {
+        Ok(())
+    }
+}
+assert_obj_safe!(FlashZero);
+
+impl<F: FlashZero> FlashZero for &mut F {
+    #[inline]
+    fn read_zerocopy(&self, slice: FlashSlice) -> Result<&[u8], Error> {
+        F::read_zerocopy(self, slice)
+    }
+
+    #[inline]
+    fn reset(&mut self) -> Result<(), Error> {
+        F::reset(self)
+    }
+}
+
+/// Shim for supporting "zero-copy" reads on a [`Flash`] via an [`Arena`].
+///
+/// This type wraps a [`Flash`] type, plus an [`Arena`], and implements
+/// zero-copy reads by first allocating a buffer on the arena and then
+/// using that allocation to perform a read.
+///
+/// [`Flash`]: trait.Flash.html
+/// [`Arena`]: ../../mem/trait.Arena.html
+pub struct ArenaFlash<F, A>(pub F, pub A);
+
+impl<F: Flash, A> Flash for ArenaFlash<F, A> {
+    #[inline]
+    fn size(&self) -> Result<u32, Error> {
+        self.0.size()
+    }
+
+    #[inline]
+    fn read(&self, offset: FlashPtr, out: &mut [u8]) -> Result<(), Error> {
+        self.0.read(offset, out)
+    }
+
+    #[inline]
+    fn program(&mut self, offset: FlashPtr, buf: &[u8]) -> Result<(), Error> {
+        self.0.program(offset, buf)
+    }
+
+    #[inline]
+    fn flush(&mut self) -> Result<(), Error> {
+        self.0.flush()
+    }
+}
+
+impl<F: Flash, A: Arena> FlashZero for ArenaFlash<F, A> {
+    fn read_zerocopy(&self, slice: FlashSlice) -> Result<&[u8], Error> {
+        let Self(flash, arena) = self;
+        let buf = arena
+            .alloc(slice.len as usize)
+            .map_err(|_| Error::Internal)?;
+        flash.read(slice.ptr, buf)?;
+        Ok(buf)
+    }
+
+    #[inline]
+    fn reset(&mut self) -> Result<(), Error> {
+        Ok(self.1.reset())
+    }
+}
+
+/// A [`Read`]/[`Write`] implementation for operating on a [`Flash`] serially.
+///
+/// [`Read`]: ../../io/read/trait.Read.html
+/// [`Write`]: ../../io/write/trait.Write.html
+/// [`Flash`]: trait.Flash.html
+pub struct FlashIo<F> {
+    flash: F,
+    cursor: u32,
+    len: u32,
+}
+
+impl<F: Flash> FlashIo<F> {
+    /// Creates a new `FlashIo`, reading/writing from the beginning of `flash`.
+    pub fn new(flash: F) -> Result<Self, Error> {
+        let len = flash.size()?;
+        Ok(Self {
+            flash,
+            cursor: 0,
+            len,
+        })
+    }
+
+    /// Skips the cursor `bytes` bytes forward.
+    ///
+    /// This operation always succeeds, but attempting to read past the end of
+    /// flash will always result in an error.
+    pub fn skip(&mut self, bytes: usize) {
+        self.cursor = self.cursor.saturating_add(bytes as u32);
+    }
+}
+
+impl<F: Flash> io::Read for FlashIo<F> {
+    fn read_bytes(&mut self, out: &mut [u8]) -> Result<(), io::Error> {
+        self.flash
+            .read(FlashPtr::new(self.cursor), out)
+            .map_err(|_| io::Error::Internal)?;
+        self.cursor += out.len() as u32;
+        Ok(())
+    }
+
+    fn remaining_data(&self) -> usize {
+        self.len.saturating_sub(self.cursor) as usize
+    }
+}
+
+impl<F: Flash> io::Write for FlashIo<F> {
+    fn write_bytes(&mut self, buf: &[u8]) -> Result<(), io::Error> {
+        self.flash
+            .program(FlashPtr::new(self.cursor), buf)
+            .map_err(|_| io::Error::Internal)?;
+        self.cursor += buf.len() as u32;
+        Ok(())
+    }
 }
 
 /// An abstract pointer into a [`Flash`] type.
@@ -100,23 +304,43 @@ impl FlashSlice {
 pub struct OutOfBounds;
 
 impl Flash for [u8] {
-    type Error = OutOfBounds;
-
-    fn size(&self) -> Result<u32, Self::Error> {
-        Ok(self.len() as u32)
+    fn size(&self) -> Result<u32, Error> {
+        self.len().try_into().map_err(|_| Error::Unspecified)
     }
 
-    fn read(
-        &self,
-        slice: FlashSlice,
-        out: &mut [u8],
-    ) -> Result<(), Self::Error> {
-        let start = slice.ptr.address as usize;
-        let end = start + slice.len as usize;
-        if start >= self.len() || end > self.len() {
-            return Err(OutOfBounds);
+    fn read(&self, offset: FlashPtr, out: &mut [u8]) -> Result<(), Error> {
+        let start = offset.address as usize;
+        let end = start.checked_add(out.len()).ok_or(Error::OutOfRange)?;
+        if end >= self.len() {
+            return Err(Error::OutOfRange);
         }
+
         out.copy_from_slice(&self[start..end]);
         Ok(())
+    }
+
+    fn program(&mut self, offset: FlashPtr, buf: &[u8]) -> Result<(), Error> {
+        let start = offset.address as usize;
+        let end = start.checked_add(buf.len()).ok_or(Error::OutOfRange)?;
+        if end >= self.len() {
+            return Err(Error::OutOfRange);
+        }
+
+        self[start..end].copy_from_slice(buf);
+        Ok(())
+    }
+}
+
+impl FlashZero for [u8] {
+    fn read_zerocopy(&self, offset: FlashSlice) -> Result<&[u8], Error> {
+        let start = offset.ptr.address as usize;
+        let end = start
+            .checked_add(offset.len as usize)
+            .ok_or(Error::OutOfRange)?;
+        if end >= self.len() {
+            return Err(Error::OutOfRange);
+        }
+
+        Ok(&self[start..end])
     }
 }
