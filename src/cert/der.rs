0bb695d4fe7c4aeffc7610092797a141f732cdf1
Miguel Young de la Sota <mcyoung@google.com>
2021-06-10 16:12:56 -0400

    [cert] Add an initial X.509 parser
    This parser will be used both for Cerberus compilance (e.g. RIoT
    certificates) and future CBOR-based formats.

---
+++ src/cert/der.rs
@@ -0,0 +1,365 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+//! DER parsing.
+//!
+//! Based on BoringSSL's DER parser. See
+//! https://boringssl.googlesource.com/boringssl/+/refs/heads/master/crypto/bytestring/cbs.c
+//!
+//! We adapt BoringSSL's parser because it is battle-tested, and because DER
+//! and X.509 are a bit fussy, so owning this dependency directly rather than
+//! trusting an external dependency is useful.
+//!
+//! Note that we reject all non-DER BER, unlike BoringSSL.
+
+#![allow(unused)]
+
+use crate::cert;
+use crate::cert::Error;
+use crate::io;
+use crate::io::Read as _;
+
+pub const TRUE: &[u8] = &[0xff];
+pub const FALSE: &[u8] = &[0x00];
+
+/// Parse `count` big-endian bytes.
+fn be(buf: &mut untrusted::Reader, count: usize) -> Result<u32, Error> {
+    debug_assert!(count <= 4);
+    let mut val = 0;
+    for _ in 0..count {
+        val <<= 8;
+        val |= buf.read_byte()? as u32;
+    }
+    Ok(val)
+}
+
+/// A DER tag.
+///
+/// Unlike BoringSSL, we don't bother to parse tag numbers greater than 30,
+/// because none of the tags we care about use a tag larger than that.
+///
+/// Tags cannot be interrogated beyond basic comparisons with existing
+/// constants.
+// This is encoded directly as the "first octet" of a DER tag.
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct Tag(u8);
+
+impl Tag {
+    /// Parses a tag.
+    fn parse(buf: &mut untrusted::Reader) -> Result<Self, Error> {
+        let tag_byte = buf.read_byte()?;
+        // We don't support extended tags.
+        if tag_byte & 0x1f == 0x1f {
+            return Err(Error::BadEncoding);
+        }
+        Ok(Self(tag_byte))
+    }
+
+    pub const BOOLEAN: Tag = Tag(0x01);
+    pub const INTEGER: Tag = Tag(0x02);
+    pub const BIT_STRING: Tag = Tag(0x03);
+    pub const OCTET_STRING: Tag = Tag(0x04);
+    pub const NULL: Tag = Tag(0x05);
+    pub const OID: Tag = Tag(0x06);
+    pub const SEQUENCE: Tag = Tag(0x30); // Constructed bit set.
+
+    /// Returns a context-specific, constructed tag.
+    #[allow(clippy::unusual_byte_groupings)]
+    pub const fn context_specific(number: u8) -> Self {
+        Self((number & 0b11111) | 0b10_1_00000)
+    }
+}
+
+/// Parse a single element of a `SEQUENCE`, returning its tag and contents.
+pub fn any<'cert>(
+    buf: &mut untrusted::Reader<'cert>,
+) -> Result<(Tag, untrusted::Input<'cert>), Error> {
+    let tag = Tag::parse(buf)?;
+    let len_byte = buf.read_byte()?;
+    // NOTE: the header is always two bytes long.
+
+    // If `len` has the high bit set, then it is a "long form" length.
+    let len = if len_byte & 0x80 == 0 {
+        len_byte as usize
+    } else {
+        let num_bytes = len_byte & 0x7f;
+
+        if num_bytes == 0 || num_bytes > 4 {
+            // We only support lengths at most 32 bits.
+            //
+            // This also catches indefinite-length constructed objects,
+            // which we absolutely don't support.
+            return Err(Error::BadEncoding);
+        }
+
+        let len = be(buf, num_bytes as usize)?;
+        if len < 128 {
+            // This should have been a short-form encoding.
+            return Err(Error::BadEncoding);
+        }
+        if len >> ((num_bytes - 1) * 8) == 0 {
+            // Superfluous zero bytes; the encoding was not
+            // minimal.
+            return Err(Error::BadEncoding);
+        }
+        len as usize
+    };
+
+    let data = buf.read_bytes(len)?;
+    Ok((tag, data))
+}
+
+/// Parses an optional element of a `SEQUENCE`.
+pub fn opt<'cert>(
+    tag: Tag,
+    buf: &mut untrusted::Reader<'cert>,
+) -> Result<Option<untrusted::Input<'cert>>, Error> {
+    if !buf.peek(tag.0) {
+        return Ok(None);
+    }
+    let (_, data) = any(buf)?;
+    Ok(Some(data))
+}
+
+/// Parses a required element of a `SEQUENCE`.
+pub fn parse<'cert>(
+    tag: Tag,
+    buf: &mut untrusted::Reader<'cert>,
+) -> Result<untrusted::Input<'cert>, Error> {
+    opt(tag, buf)?.ok_or(Error::BadEncoding)
+}
+
+/// Parses a required element of a `SEQUENCE`, passing the buffer to `dec` for further decoding.
+#[inline]
+pub fn tagged<'cert, T>(
+    tag: Tag,
+    buf: &mut untrusted::Reader<'cert>,
+    dec: impl FnOnce(&mut untrusted::Reader<'cert>) -> Result<T, Error>,
+) -> Result<T, Error> {
+    parse(tag, buf)?.read_all(Error::BadEncoding, dec)
+}
+
+/// Parses a `BIT STRING`, ensuring that its length is divisible by 8.
+#[inline]
+pub fn bits_total<'cert>(
+    buf: &mut untrusted::Reader<'cert>,
+) -> Result<untrusted::Input<'cert>, Error> {
+    bits_checked(buf, true)
+}
+
+/// Parses a `BIT STRING`, ensuring that its trailing bits are zero.
+#[inline]
+pub fn bits_partial<'cert>(
+    buf: &mut untrusted::Reader<'cert>,
+) -> Result<untrusted::Input<'cert>, Error> {
+    bits_checked(buf, true)
+}
+
+fn bits_checked<'cert>(
+    buf: &mut untrusted::Reader<'cert>,
+    ensure_octets: bool,
+) -> Result<untrusted::Input<'cert>, Error> {
+    tagged(Tag::BIT_STRING, buf, |buf| {
+        // A bit string is lead by a bit specifying how much padding the
+        // string has.
+        match buf.read_byte()? {
+            // Zero is always ok.
+            0 => {
+                if buf.at_end() {
+                    return Err(Error::BadEncoding);
+                }
+                Ok(buf.read_bytes_to_end())
+            }
+
+            // If we specifically requested octets, reject extra padding.
+            _ if ensure_octets => return Err(Error::BadEncoding),
+
+            extra @ 1..=7 => {
+                let rest = buf.read_bytes_to_end();
+                let last = *rest
+                    .as_slice_less_safe()
+                    .last()
+                    .ok_or(Error::BadEncoding)?;
+
+                // Check that the top `extra` bits are all zero.
+                let mask = (1 << extra) - 1;
+                if (last & mask != 0) {
+                    return Err(Error::BadEncoding);
+                }
+
+                Ok(rest)
+            }
+
+            // Too many extra bits.
+            _ => return Err(Error::BadEncoding),
+        }
+    })
+}
+
+/// Parses a non-negative `INTEGER`.
+///
+/// We reject all integers with the sign bit set.
+pub fn uint<'cert>(
+    buf: &mut untrusted::Reader<'cert>,
+) -> Result<untrusted::Input<'cert>, Error> {
+    let (tag, data) = any(buf)?;
+    if tag != Tag::INTEGER {
+        return Err(Error::BadEncoding);
+    }
+    data.read_all(Error::BadEncoding, |buf| {
+        // `data` must be non-empty.
+        let first = buf.read_byte()?;
+
+        // `[0x00]` is the valid representation of `0`.
+        if first == 0 && buf.at_end() {
+            return Ok(());
+        }
+
+        // Check the sign bit; negative values are forbidden.
+        if first & 0x80 != 0 {
+            return Err(Error::BadEncoding);
+        }
+
+        // Leading nonzero is always ok.
+        if first != 0 {
+            return Ok(());
+        }
+
+        // A leading zero is only permitted if the byte that
+        // follows has a high bit set, to disambiguate
+        // `-128 == [0x80]` from `128 == [0x00, 0x80]`.
+        if buf.read_byte()? & 0x80 == 0 {
+            return Err(Error::BadEncoding);
+        }
+
+        buf.skip_to_end();
+        Ok(())
+    })?;
+    Ok(data)
+}
+
+/// Parses an non-negative `INTEGER`, up to four bytes.
+pub fn u32(buf: &mut untrusted::Reader) -> Result<u32, Error> {
+    uint(buf)?.read_all(Error::BadEncoding, |buf| {
+        let mut v: u32 = 0;
+        while let Ok(b) = buf.read_byte() {
+            v = v.checked_shl(8).ok_or(Error::BadEncoding)?;
+            v |= b as u32;
+        }
+        Ok(v)
+    })
+}
+
+/// Parses an optional, non-negative `INTEGER`, up to four bytes.
+pub fn opt_u32(buf: &mut untrusted::Reader) -> Result<Option<u32>, Error> {
+    if !buf.peek(Tag::INTEGER.0) {
+        return Ok(None);
+    }
+    u32(buf).map(Some)
+}
+
+/// Parses a `NULL`.
+pub fn null(buf: &mut untrusted::Reader) -> Result<(), Error> {
+    tagged(Tag::NULL, buf, |mut buf| {
+        if buf.at_end() {
+            Ok(())
+        } else {
+            Err(Error::BadEncoding)
+        }
+    })
+}
+
+/// Parses an `OBJECT IDENTIFIER`. The only purpose of an OID is to be compared
+/// to other OIDs byte-for-byte.
+pub fn oid<'cert>(
+    buf: &mut untrusted::Reader<'cert>,
+) -> Result<Oid<'cert>, Error> {
+    Ok(Oid::new(parse(Tag::OID, buf)?.as_slice_less_safe()))
+}
+
+pub fn opt_bool(buf: &mut untrusted::Reader) -> Result<Option<bool>, Error> {
+    match &opt(Tag::BOOLEAN, buf)? {
+        None => Ok(None),
+        Some(b) if b == FALSE => Ok(Some(false)),
+        Some(b) if b == TRUE => Ok(Some(true)),
+        _ => Err(Error::BadEncoding),
+    }
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct Oid<'cert>(&'cert [u8]);
+
+impl<'cert> Oid<'cert> {
+    pub const fn new(der: &'cert [u8]) -> Self {
+        Self(der)
+    }
+}
+
+/// Generates a new [`Oid`] constant with the given components.
+macro_rules! oid {
+    ($($val:literal),* $(,)?) => {{
+        const LEN: usize = [$($val),*].len();
+        const OID: [u32; LEN] = [$($val),*];
+        const ENC_RAW: ([u8; LEN * 4], usize) = {
+            let mut buf = [0u8; LEN * 4];
+            let mut len = 0;
+
+            // An OID is an array of at least two integers encoded as follows:
+            // - First, a single byte containing the first two integers,
+            //   encoded by the line below.
+            // - Then, for each integer, encode it in base 128 (i.e., seven
+            //   bits to a byte) in big-endian order. All bytes except the
+            //   last have the high bit set.
+            buf[0] = (OID[0] * 40 + OID[1]) as u8;
+            len += 1;
+
+            let mut idx = 2;
+            while idx < LEN {
+                let mut c = OID[idx];
+                idx += 1;
+
+                // Encode the component in base 128, but in
+                // little-endian order because that's easier.
+                // We then reverse it after the fact below.
+                let mark = len;
+                let mut first = true;
+                while c != 0 {
+                    buf[len] = (c & 0x7f) as u8;
+                    if !first {
+                        buf[len] |= 0x80;
+                    }
+
+                    c >>= 7;
+                    first = false;
+                    len += 1;
+                }
+
+                // No `const` reverse() yet, so we have to do it by hand.
+                let mut idx = 0;
+                #[allow(clippy::manual_swap)]
+                while idx < (len - mark) / 2 {
+                    let tmp = buf[mark + idx];
+                    buf[mark + idx] = buf[len - idx - 1];
+                    buf[len - idx - 1] = tmp;
+                    idx += 1;
+                }
+            }
+            (buf, len)
+        };
+
+        // Slicing is currently not available in `const`, so we simply copy
+        // the array to another one of the correct size.
+        const ENC: [u8; ENC_RAW.1] = {
+            let mut buf = [0u8; ENC_RAW.1];
+            let mut idx = 0;
+            while idx < buf.len() {
+                buf[idx] = ENC_RAW.0[idx];
+                idx += 1;
+            }
+            buf
+        };
+
+        $crate::cert::der::Oid::<'static>::new(&ENC)
+    }}
+}
---
+++ src/cert/x509.rs
@@ -0,0 +1,278 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+//! X.509 parsing.
+
+use crate::cert;
+use crate::cert::der;
+use crate::cert::der::Tag;
+use crate::cert::Algo;
+use crate::cert::Cert;
+use crate::cert::Error;
+use crate::cert::Name;
+use crate::cert::PublicKeyParams;
+
+#[cfg(test)]
+#[path = "x509_test.rs"]
+mod test;
+
+/// OIDs used by the parser.
+#[allow(unused)]
+mod oid {
+    use crate::cert::der::Oid;
+
+    pub const RSA_ENCRYPTION: Oid = oid!(1, 2, 840, 113549, 1, 1, 1);
+    pub const RSA_PKCS1_SHA256: Oid = oid!(1, 2, 840, 113549, 1, 1, 11);
+
+    pub const KEY_USAGE: Oid = oid!(2, 5, 29, 15);
+    pub const BASIC_CONSTRAINTS: Oid = oid!(2, 5, 29, 19);
+    pub const TCG_DICE_FWID: Oid = oid!(2, 23, 133, 5, 4, 1);
+}
+
+/// Parses an RFC3279 algorithm identifier.
+fn parse_algo(buf: &mut untrusted::Reader) -> Result<Algo, Error> {
+    match der::oid(buf)? {
+        oid::RSA_PKCS1_SHA256 => {
+            der::null(buf)?;
+            Ok(Algo::RsaPkcs1Sha256)
+        }
+        _ => Err(Error::UnknownAlgorithm),
+    }
+}
+
+/// Parses an X.509 certificate.
+///
+/// This function performs several aggressive checks to reject any and all
+/// certificates that do not fit our profile. These include:
+/// - Version must be v3.
+/// - Extensions must be present.
+/// - `keyUsage`, `authorityKeyIdentifier`, `subjectKeyIdentifier` must all
+///   be present.
+///   - A `keyUsage` with `keyCertSign` and any other usage is rejected.
+///
+/// All of the above are treated as encoding errors.
+pub fn parse<'cert>(
+    cert: &'cert [u8],
+    format: cert::CertFormat,
+    key: Option<&PublicKeyParams<'_>>,
+    ciphers: &mut impl cert::Ciphers,
+) -> Result<Cert<'cert>, Error> {
+    let buf = untrusted::Input::from(cert);
+    let (cert, tbs, sig_algo, sig) =
+        buf.read_all(Error::BadEncoding, |buf| {
+            der::tagged(Tag::SEQUENCE, buf, |buf| {
+                let mark = buf.mark();
+                let tbs = der::parse(Tag::SEQUENCE, buf)?;
+                let tbs_bytes =
+                    buf.get_input_between_marks(mark, buf.mark())?;
+                let sig_algo_bytes = der::parse(Tag::SEQUENCE, buf)?;
+                let sig_algo =
+                    sig_algo_bytes.read_all(Error::BadEncoding, parse_algo)?;
+
+                let sig = der::bits_total(buf)?;
+
+                let cert = tbs.read_all(Error::BadEncoding, |buf| {
+                    parse_tbs(format, sig_algo_bytes, buf)
+                })?;
+
+                Ok((
+                    cert,
+                    tbs_bytes.as_slice_less_safe(),
+                    sig_algo,
+                    sig.as_slice_less_safe(),
+                ))
+            })
+        })?;
+
+    let key = key.unwrap_or_else(|| cert.subject_key());
+    if !key.is_params_for(sig_algo) {
+        return Err(Error::WrongAlgorithm);
+    }
+
+    let verifier = ciphers
+        .verifier(sig_algo, key)
+        .ok_or(Error::UnknownAlgorithm)?;
+    verifier.verify(sig, tbs).map_err(|_| Error::BadSignature)?;
+
+    Ok(cert)
+}
+
+fn parse_tbs<'cert>(
+    format: cert::CertFormat,
+    sig_algo_bytes: untrusted::Input,
+    buf: &mut untrusted::Reader<'cert>,
+) -> Result<Cert<'cert>, Error> {
+    // Although the version field is optional, we reject all non-v3
+    // certificates, which require this field.
+    //
+    // We treat this as a syntactic, rather than semantic, error.
+    der::tagged(Tag::context_specific(0), buf, |mut buf| {
+        // `v3` certificates are encoded as an `INTEGER { 2 }`.
+        if der::u32(&mut buf)? != 2 {
+            return Err(Error::BadEncoding);
+        }
+        Ok(())
+    })?;
+
+    // The certificate serial number must be a positive `INTEGER` consisting
+    // of at most 20 octets.
+    //
+    // Like with the version, this is a syntactic error. The value itself
+    // is discarded.
+    let serial = der::uint(buf)?.as_slice_less_safe();
+    if serial[0] == 0 || serial.len() > 20 {
+        return Err(Error::BadEncoding);
+    }
+
+    // A mismatch between the inner and outer signature algorithm
+    // identifiers (byte-for-byte) is a syntax error.
+    let sig_algo2 = der::parse(Tag::SEQUENCE, buf)?;
+    if sig_algo2 != sig_algo_bytes {
+        return Err(Error::BadEncoding);
+    }
+
+    // The issuer is an opaque name.
+    let issuer = Name(der::parse(Tag::SEQUENCE, buf)?.as_slice_less_safe());
+
+    // TODO: Provide some mechanism for the user to pass in a clock, if
+    // available. X.509 time parsing incurs significant complexity, so
+    // it probably shouldn't be implemented until it's absolutely necessary.
+    let _validity = der::parse(Tag::SEQUENCE, buf)?;
+
+    // The subject is also opaque
+    let subject = Name(der::parse(Tag::SEQUENCE, buf)?.as_slice_less_safe());
+
+    let subject_key = der::tagged(Tag::SEQUENCE, buf, |buf| {
+        let (algo, aparams) = der::tagged(Tag::SEQUENCE, buf, |buf| {
+            let algo = der::oid(buf)?;
+            let aparams = buf.read_bytes_to_end();
+            Ok((algo, aparams))
+        })?;
+
+        der::bits_total(buf)?.read_all(Error::BadEncoding, |buf| match algo {
+            oid::RSA_ENCRYPTION => {
+                aparams.read_all(Error::BadEncoding, der::null)?;
+                der::tagged(Tag::SEQUENCE, buf, |buf| {
+                    let mut modulus = der::uint(buf)?.as_slice_less_safe();
+                    // DER inserts a leading zero sometimes (to disambiguate
+                    // negative integers) so we need to remove it.
+                    if modulus[0] == 0 {
+                        modulus = &modulus[1..];
+                    }
+                    let mut exponent = der::uint(buf)?.as_slice_less_safe();
+                    if exponent[0] == 0 {
+                        exponent = &exponent[1..];
+                    }
+                    Ok(PublicKeyParams::Rsa { modulus, exponent })
+                })
+            }
+            _ => Err(Error::UnknownAlgorithm),
+        })
+    })?;
+
+    // We don't care about the UIDs at all.
+    let _issuer_uid = der::opt(Tag::context_specific(1), buf)?;
+    let _subject_uid = der::opt(Tag::context_specific(2), buf)?;
+
+    // Extensions are mandatory.
+    let mut extns = Extensions::default();
+    der::tagged(Tag::context_specific(3), buf, |buf| {
+        der::tagged(Tag::SEQUENCE, buf, |buf| {
+            while !buf.at_end() {
+                parse_extn(buf, &mut extns)?;
+            }
+            Ok(())
+        })
+    })?;
+
+    let is_cert_sign = match extns.is_cert_sign {
+        Some(b) => b,
+        _ => return Err(Error::BadEncoding),
+    };
+
+    let is_ca = match &extns.basic_constraints {
+        Some(bc) => bc.is_ca,
+        _ => false,
+    };
+
+    // CA certificates must always specify keyCertSign as a valid usage. To
+    // fail to do so is a syntax error.
+    if is_ca != is_cert_sign {
+        return Err(Error::BadEncoding);
+    }
+
+    Ok(Cert {
+        format,
+        issuer,
+        subject,
+        subject_key,
+        basic_constraints: extns.basic_constraints,
+        is_cert_sign,
+    })
+}
+
+#[derive(Default)]
+struct Extensions {
+    basic_constraints: Option<cert::BasicConstraints>,
+    is_cert_sign: Option<bool>,
+}
+
+fn parse_extn(
+    buf: &mut untrusted::Reader,
+    extns: &mut Extensions,
+) -> Result<(), Error> {
+    der::tagged(Tag::SEQUENCE, buf, |buf| {
+        let oid = der::oid(buf)?;
+        let is_critical = der::opt_bool(buf)?.unwrap_or(false);
+        der::tagged(Tag::OCTET_STRING, buf, |buf| match oid {
+            oid::KEY_USAGE => {
+                if extns.is_cert_sign.is_some() {
+                    return Err(Error::BadEncoding);
+                }
+
+                // Note that the bits in a `BIT STRING` are big endian
+                // within bytes, so we need to "shift from the left"
+                // instead.
+                const CERT_SIGN_MASK: u16 = 1 << (15 - 5);
+
+                der::bits_partial(buf)?.read_all(Error::BadEncoding, |buf| {
+                    let first_byte = buf.read_byte().unwrap_or(0);
+                    let second_byte = buf.read_byte().unwrap_or(0);
+                    // NOTE: This technically drops any "domain-specific"
+                    // bits on the ground, but we have zero interest in retaining
+                    // those.
+                    let bits = u16::from_be_bytes([first_byte, second_byte]);
+
+                    extns.is_cert_sign = Some(bits & CERT_SIGN_MASK != 0);
+                    if extns.is_cert_sign == Some(true)
+                        && bits != CERT_SIGN_MASK
+                    {
+                        // For domain separation reasons, we reject all
+                        // certificates that mix certificate signing with any
+                        // other usage.
+                        return Err(Error::BadEncoding);
+                    }
+
+                    Ok(())
+                })
+            }
+            oid::BASIC_CONSTRAINTS => {
+                if extns.basic_constraints.is_some() {
+                    return Err(Error::BadEncoding);
+                }
+                der::tagged(Tag::SEQUENCE, buf, |buf| {
+                    let is_ca = der::opt_bool(buf)?.unwrap_or(false);
+                    let path_len_constraint = der::opt_u32(buf)?;
+                    extns.basic_constraints = Some(cert::BasicConstraints {
+                        is_ca,
+                        path_len_constraint,
+                    });
+                    Ok(())
+                })
+            }
+            _ if is_critical => Err(Error::BadEncoding),
+            _ => Ok(()),
+        })
+    })
+}
