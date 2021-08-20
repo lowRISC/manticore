dc8913390f3b3252fae14038d53235ddab3d8dc7
Miguel Young de la Sota <mcyoung@google.com>
2021-08-20 11:17:46 -0400

    [cert] Add a function for generating test CWT certs
    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>

--- src/cert/cwt/test.rs
+++ src/cert/cwt/test.rs
@@ -11,6 +11,7 @@ use testutil::data::keys;
 use crate::cert::Cert;
 use crate::cert::CertFormat;
 use crate::crypto::ring;
+use crate::crypto::sig::PublicKeyParams;
 use crate::crypto::sig::Sign as _;
 
 const UINT: u8 = 0;
@@ -20,71 +21,83 @@ const BYTES: u8 = 2;
 const ARRAY: u8 = 4;
 const MAP: u8 = 5;
 
+/// Generator for a test CWT.
+pub struct TestCwt {
+    issuer: &'static str,
+    subject: &'static str,
+    spki: PublicKeyParams<'static>,
+    key_usage: &'static [u8],
+    issuer_key: &'static [u8],
+}
+
+impl TestCwt {
+    pub fn encode(&self) -> Vec<u8> {
+        let protected = raw_cbor!(MAP [
+            // Algorithm, i.e., RSA.
+            UINT:1  NINT:256,
+            // Criticalities.
+            UINT:2  ARRAY[UINT:1, UINT:2],
+        ]);
+
+        let spki = match &self.spki {
+            PublicKeyParams::Rsa { modulus, exponent } => raw_cbor!(MAP [
+                // Key type, RSA.
+                UINT:1  UINT:3,
+                // Algorithm, RSA.
+                UINT:3  NINT:256,
+                // Modulus.
+                NINT:0  BYTES {modulus},
+                // Exponent.
+                NINT:1  BYTES {exponent},
+            ]),
+        };
+
+        let payload = raw_cbor!(MAP [
+            // Issuer.
+            UINT:1          UTF8 {(self.issuer)},
+            // Subject.
+            UINT:2          UTF8 {(self.subject)},
+            // DICE SPKI.
+            NINT:4670551    spki,
+            // DICE KeyUsage
+            NINT:4670552    BYTES {(self.key_usage)},
+        ]);
+
+        let tbs = raw_cbor!(ARRAY [
+            UTF8 {"Signature1"},
+            BYTES {protected},
+            BYTES {},
+            BYTES {payload},
+        ]);
+
+        let (_, mut signer) = ring::rsa::from_keypair(self.issuer_key);
+        let mut sig = vec![0; signer.sig_bytes()];
+        signer.sign(&[&tbs], &mut sig).unwrap();
+
+        raw_cbor! {
+            BYTES {protected}
+            MAP []  // No unprotected fields.
+            BYTES {payload}
+            BYTES {sig}
+        }
+    }
+}
+
 #[test]
 #[cfg_attr(miri, ignore)]
 fn self_signed() {
-    let protected = raw_cbor!(MAP [
-        // Algorithm.
-        UINT:1  NINT:256,
-        // Criticalities.
-        UINT:2  ARRAY[UINT:1, UINT:2],
-    ]);
-    let payload = raw_cbor!(MAP [
-        // Issuer.
-        UINT:1  UTF8 {"my cool ca"},
-        // Subject; self-signed.
-        UINT:2  UTF8 {"my cool ca"},
-
-        // DICE SPKI.
-        NINT:4670551    MAP [
-            // Key type, RSA.
-            UINT:1  UINT:3,
-            // Algorithm,i RSA.
-            UINT:3  NINT:256,
-            // Modulus.
-            NINT:0  BYTES {h"
-                a6be96e18c5c826a748b887b194ed3f6
-                daaa8897cdbaee69e5a980b9dab5af7e
-                5f4fed0236d78400837b28229fd4ee24
-                1c0d6f3178651265a2762aa1f449dcec
-                2a8b679058f9c97a3ba93a7c2bb6d62d
-                3baaa70a47991ee6f0daefc55892035f
-                a4f4d1c493a73bf61018c6cf86bb43aa
-                151f9f0bfa5f474a7fa2bba6a1c77e17
-                e160cee2387586e43ff78efd95327d22
-                0fc12b0e717089b18cf667ce672d6351
-                d187d9330b8777cbe2fb28fccba0b2ad
-                c560c9d03e2cba50d624736545c587cc
-                0f1d3ef3d7ec69f4108fff270455155b
-                d9a3d72c41a397915bad3c9aba753e7b
-                f91614d25642cd968816108647e2f7a8
-                580e4c05ff0a7f233beba82ca8569a53
-            "},
-            // Exponent.
-            NINT:1  BYTES {h"010001"},
-        ],
-        // DICE KeyUsage
-        NINT:4670552    BYTES {[0b0010_0000]},
-    ]);
-
-    let tbs = raw_cbor!(ARRAY [
-        UTF8 {"Signature1"},
-        BYTES {protected},
-        BYTES {},
-        BYTES {payload},
-    ]);
-
-    let (_, mut signer) = ring::rsa::from_keypair(keys::KEY1_RSA_KEYPAIR);
-    let mut sig = vec![0; signer.sig_bytes()];
-    signer.sign(&[&tbs], &mut sig).unwrap();
-
-    let data = raw_cbor! {
-        BYTES {protected}
-        MAP []
-        BYTES {payload}
-        BYTES {sig}
+    let cwt = TestCwt {
+        issuer: "my cool ca",
+        subject: "my cool ca",
+        spki: PublicKeyParams::Rsa {
+            modulus: keys::KEY1_RSA_MOD,
+            exponent: keys::KEY1_RSA_EXP,
+        },
+        key_usage: &[0b0010_0000],
+        issuer_key: keys::KEY1_RSA_KEYPAIR,
     };
 
+    let data = cwt.encode();
     let cert = Cert::parse(
         &data,
         CertFormat::OpenDiceCwt,
