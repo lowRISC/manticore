dda6e025e6179e3f13b9b22a25052992a2b96c03
Miguel Young de la Sota <mcyoung@google.com>
2021-08-20 11:17:04 -0400

    [testutil] Add constants for the raw mod/exp of RSA keys
    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>

--- testutil/src/data/keys.rs
+++ testutil/src/data/keys.rs
@@ -12,6 +12,12 @@ pub const KEY1_RSA_KEYPAIR: &[u8] = include_bytes!("keys/key1.rsa.pk8");
 /// Test-only RSA public key generated from `key1.rsa.pk8`.
 #[rustfmt::skip]
 pub const KEY1_RSA_PUBLIC: &[u8] = include_bytes!("keys/key1.rsa.pub.pk8");
+/// RSA modulus of `key1.rsa.pk8`.
+#[rustfmt::skip]
+pub const KEY1_RSA_MOD: &[u8] = include_bytes!("keys/generated/key1.rsa.pub.mod");
+/// RSA exponent of `key1.rsa.pk8`.
+#[rustfmt::skip]
+pub const KEY1_RSA_EXP: &[u8] = include_bytes!("keys/generated/key1.rsa.pub.exp");
 
 /// Test-only RSA keypair `key2.rsa.pk8`.
 #[rustfmt::skip]
@@ -19,3 +25,22 @@ pub const KEY2_RSA_KEYPAIR: &[u8] = include_bytes!("keys/key2.rsa.pk8");
 /// Test-only RSA public key generated from `key2.rsa.pk8`.
 #[rustfmt::skip]
 pub const KEY2_RSA_PUBLIC: &[u8] = include_bytes!("keys/key2.rsa.pub.pk8");
+/// RSA modulus of `key2.rsa.pk8`.
+#[rustfmt::skip]
+pub const KEY2_RSA_MOD: &[u8] = include_bytes!("keys/generated/key2.rsa.pub.mod");
+/// RSA exponent of `key2.rsa.pk8`.
+#[rustfmt::skip]
+pub const KEY2_RSA_EXP: &[u8] = include_bytes!("keys/generated/key2.rsa.pub.exp");
+
+/// Test-only RSA keypair `key3.rsa.pk8`.
+#[rustfmt::skip]
+pub const KEY3_RSA_KEYPAIR: &[u8] = include_bytes!("keys/key3.rsa.pk8");
+/// Test-only RSA public key generated from `key3.rsa.pk8`.
+#[rustfmt::skip]
+pub const KEY3_RSA_PUBLIC: &[u8] = include_bytes!("keys/key3.rsa.pub.pk8");
+/// RSA modulus of `key3.rsa.pk8`.
+#[rustfmt::skip]
+pub const KEY3_RSA_MOD: &[u8] = include_bytes!("keys/generated/key3.rsa.pub.mod");
+/// RSA exponent of `key3.rsa.pk8`.
+#[rustfmt::skip]
+pub const KEY3_RSA_EXP: &[u8] = include_bytes!("keys/generated/key3.rsa.pub.exp");
