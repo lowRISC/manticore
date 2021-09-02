1262cfd95b1236dc70c4890780219438852846ce
Miguel Young de la Sota <mcyoung@google.com>
2021-09-02 11:45:41 -0400

    [fuzz] Add fuzz tests for KeyExchange
    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>

---
+++ fuzz/gen/manticore_protocol_key_exchange_KeyExchange__req_to_wire.rs
@@ -0,0 +1,24 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// !! DO NOT EDIT !!
+// To regenerate this file, run `fuzz/generate_proto_tests.py`.
+
+#![no_main]
+#![allow(non_snake_case)]
+
+use libfuzzer_sys::fuzz_target;
+
+use manticore::protocol::Command;
+use manticore::protocol::wire::ToWire;
+use manticore::protocol::FuzzSafe;
+
+use manticore::protocol::key_exchange::KeyExchange as C;
+type Req<'a> = <C as Command<'a>>::Req;
+
+fuzz_target!(|data: <Req<'static> as FuzzSafe>::Safe| {
+    let mut out = [0u8; 1024];
+    let _ = Req::from_safe(&data).to_wire(&mut &mut out[..]);
+});
+
