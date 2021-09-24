8dc6931a6de94ec80ed7945653841f49c13831ed
Miguel Young de la Sota <mcyoung@google.com>
2021-09-24 12:07:41 -0400

    [server] Move some handlers out-of-line for readability
    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>

--- src/server/pa_rot.rs
+++ src/server/pa_rot.rs
@@ -98,55 +98,19 @@ impl<'a> PaRot<'a> {
     }
 
     /// Process a single incoming request.
-    ///
-    /// The request message will be read from `req`, while the response
-    /// message will be written to `resp`.
-    #[cfg_attr(test, inline(never))]
     pub fn process_request<'req, A: Arena>(
         &mut self,
         host_port: &mut dyn net::HostPort<'req>,
         arena: &'req A,
     ) -> Result<(), Error> {
+        // Style note: when defining a new handler, if it is more than a
+        // handful of lines long, define it out-of-line instead.
         let result = Handler::<&mut Self, A>::new()
             .handle::<protocol::FirmwareVersion, _>(|ctx| {
-                use protocol::firmware_version::FirmwareVersionResponse;
-                if ctx.req.index == 0 {
-                    return Ok(FirmwareVersionResponse {
-                        version: ctx.server.opts.identity.firmware_version(),
-                    });
-                }
-
-                let version = ctx
-                    .server
-                    .opts
-                    .identity
-                    .vendor_firmware_version(ctx.req.index)
-                    .ok_or(UNSPECIFIED)?;
-                Ok(FirmwareVersionResponse { version })
+                ctx.server.handle_fw_version(&ctx.req)
             })
             .handle::<protocol::DeviceCapabilities, _>(|ctx| {
-                use protocol::capabilities::*;
-                let mut crypto = ctx.req.capabilities.crypto;
-
-                ctx.server.opts.ciphers.negotiate(&mut crypto);
-                crypto.has_aes = false;
-                crypto.aes_strength = AesKeyStrength::empty();
-
-                let capabilities = Capabilities {
-                    networking: ctx.server.opts.networking,
-                    security: Security::empty(),
-
-                    has_pfm_support: false,
-                    has_policy_support: false,
-                    has_firmware_protection: false,
-
-                    crypto,
-                };
-
-                Ok(protocol::capabilities::DeviceCapabilitiesResponse {
-                    capabilities,
-                    timeouts: ctx.server.opts.timeouts,
-                })
+                ctx.server.handle_capabilities(&ctx.req)
             })
             .handle::<protocol::DeviceId, _>(|ctx| {
                 Ok(protocol::device_id::DeviceIdResponse {
@@ -159,155 +123,17 @@ impl<'a> PaRot<'a> {
                 })
             })
             .handle::<protocol::GetDigests, _>(|ctx| {
-                let digests_len = ctx
-                    .server
-                    .opts
-                    .trust_chain
-                    .chain_len(ctx.req.slot)
-                    .ok_or(UNSPECIFIED)?
-                    .get();
-                let digests = ctx
-                    .arena
-                    .alloc_slice::<[u8; hash::Algo::Sha256.bytes()]>(
-                        digests_len,
-                    )
-                    .map_err(|_| UNSPECIFIED)?;
-                for (i, digest) in digests.iter_mut().enumerate() {
-                    let cert = ctx
-                        .server
-                        .opts
-                        .trust_chain
-                        .cert(ctx.req.slot, i)
-                        .ok_or(UNSPECIFIED)?;
-                    ctx.server
-                        .opts
-                        .hasher
-                        .contiguous_hash(hash::Algo::Sha256, cert.raw(), digest)
-                        .map_err(|_| UNSPECIFIED)?;
-                }
-
-                ctx.server.key_exchange = Some(ctx.req.key_exchange);
-                Ok(protocol::get_digests::GetDigestsResponse { digests })
+                ctx.server.handle_digests(ctx.arena, &ctx.req)
             })
             .handle::<protocol::GetCert, _>(|ctx| {
-                let cert = ctx
-                    .server
-                    .opts
-                    .trust_chain
-                    .cert(ctx.req.slot, ctx.req.cert_number as usize)
-                    .ok_or(UNSPECIFIED)?;
-
-                let start = cert.raw().len().min(ctx.req.offset as usize);
-                let end = cert
-                    .raw()
-                    .len()
-                    .min((ctx.req.len as usize).saturating_add(start));
-                Ok(protocol::get_cert::GetCertResponse {
-                    slot: ctx.req.slot,
-                    cert_number: ctx.req.cert_number,
-                    data: &cert.raw()[start..end],
-                })
+                ctx.server.handle_cert(&ctx.req)
             })
             .handle_buffered::<protocol::Challenge, _>(|ctx| {
-                use protocol::challenge::*;
-
-                let signer = ctx
-                    .server
-                    .opts
-                    .trust_chain
-                    .signer(ctx.req.slot)
-                    .ok_or(UNSPECIFIED)?;
-                let nonce: &mut [u8; 32] =
-                    ctx.arena.alloc().map_err(|_| UNSPECIFIED)?;
-                ctx.server.opts.csrng.fill(nonce).map_err(|_| UNSPECIFIED)?;
-
-                let tbs = ChallengeResponseTbs {
-                    slot: ctx.req.slot,
-                    slot_mask: 0, // Currently unspecified?
-                    protocol_range: (0, 0),
-                    nonce,
-                    pmr0_components: 0,
-                    pmr0: ctx.server.opts.pmr0,
-                };
-
-                let req_buf = ctx.req_buf;
-                let signature = ctx
-                    .arena
-                    .alloc_slice::<u8>(signer.sig_bytes())
-                    .map_err(|_| UNSPECIFIED)?;
-                let sig_len = tbs
-                    .as_iovec_with(|[a, b, c, d]| {
-                        signer.sign(&[req_buf, a, b, c, d], signature)
-                    })
-                    .map_err(|_| UNSPECIFIED)?;
-                let signature = &signature[..sig_len];
-
-                if let Some(KeyExchangeAlgo::Ecdh) = ctx.server.key_exchange {
-                    ctx.server
-                        .opts
-                        .session
-                        .create_session(ctx.req.nonce, tbs.nonce)
-                        .map_err(|_| UNSPECIFIED)?;
-                    ctx.server.current_cert_slot = Some(tbs.slot);
-                }
-
-                Ok(ChallengeResponse { tbs, signature })
+                ctx.server
+                    .handle_challenge(ctx.arena, &ctx.req, ctx.req_buf)
             })
             .handle::<protocol::KeyExchange, _>(|ctx| {
-                use protocol::key_exchange::*;
-                match &ctx.req {
-                    KeyExchangeRequest::SessionKey {
-                        hmac_algorithm: _,
-                        pk_req,
-                    } => {
-                        let signer = ctx
-                            .server
-                            .opts
-                            .trust_chain
-                            .signer(
-                                ctx.server
-                                    .current_cert_slot
-                                    .ok_or(UNSPECIFIED)?,
-                            )
-                            .ok_or(UNSPECIFIED)?;
-
-                        let pk_resp = ctx
-                            .arena
-                            .alloc_slice(
-                                ctx.server.opts.session.ephemeral_bytes(),
-                            )
-                            .map_err(|_| UNSPECIFIED)?;
-                        let key_len = ctx
-                            .server
-                            .opts
-                            .session
-                            .begin_ecdh(pk_resp)
-                            .map_err(|_| UNSPECIFIED)?;
-                        let pk_resp = &pk_resp[..key_len];
-                        ctx.server
-                            .opts
-                            .session
-                            .finish_ecdh(pk_req)
-                            .map_err(|_| UNSPECIFIED)?;
-
-                        let signature = ctx
-                            .arena
-                            .alloc_slice(signer.sig_bytes())
-                            .map_err(|_| UNSPECIFIED)?;
-                        signer
-                            .sign(&[pk_req, pk_resp], signature)
-                            .map_err(|_| UNSPECIFIED)?;
-
-                        Ok(KeyExchangeResponse::SessionKey {
-                            pk_resp,
-                            signature,
-                            alias_cert_hmac: &[
-                                // Pending require of crypto::sha256.
-                            ],
-                        })
-                    }
-                    _ => Err(UNSPECIFIED),
-                }
+                ctx.server.handle_key_xchg(ctx.arena, &ctx.req)
             })
             .handle::<protocol::ResetCounter, _>(|ctx| {
                 use protocol::reset_counter::*;
@@ -324,18 +150,17 @@ impl<'a> PaRot<'a> {
                 })
             })
             .handle::<protocol::DeviceUptime, _>(|ctx| {
-                use protocol::device_uptime::*;
-                // NOTE: CUrrently, we only handle port 0, the "self" port.
+                // NOTE: Currently, we only handle port 0, the "self" port.
                 if ctx.req.port_id != 0 {
                     return Err(UNSPECIFIED);
                 }
-                Ok(DeviceUptimeResponse {
+
+                Ok(protocol::device_uptime::DeviceUptimeResponse {
                     uptime: ctx.server.opts.reset.uptime(),
                 })
             })
             .handle::<protocol::RequestCounter, _>(|ctx| {
-                use protocol::request_counter::*;
-                Ok(RequestCounterResponse {
+                Ok(protocol::request_counter::RequestCounterResponse {
                     ok_count: ctx.server.ok_count,
                     err_count: ctx.server.err_count,
                 })
@@ -347,6 +172,206 @@ impl<'a> PaRot<'a> {
             Err(_) => self.err_count += 1,
         }
         result
+    }
+
+    fn handle_fw_version(
+        &mut self,
+        req: &protocol::firmware_version::FirmwareVersionRequest,
+    ) -> Result<
+        protocol::firmware_version::FirmwareVersionResponse,
+        protocol::Error,
+    > {
+        use protocol::firmware_version::*;
+
+        if req.index == 0 {
+            return Ok(FirmwareVersionResponse {
+                version: self.opts.identity.firmware_version(),
+            });
+        }
+
+        let version = self
+            .opts
+            .identity
+            .vendor_firmware_version(req.index)
+            .ok_or(UNSPECIFIED)?;
+        Ok(FirmwareVersionResponse { version })
+    }
+
+    fn handle_capabilities(
+        &mut self,
+        req: &protocol::capabilities::DeviceCapabilitiesRequest,
+    ) -> Result<
+        protocol::capabilities::DeviceCapabilitiesResponse,
+        protocol::Error,
+    > {
+        use protocol::capabilities::*;
+        let mut crypto = req.capabilities.crypto;
+
+        self.opts.ciphers.negotiate(&mut crypto);
+        crypto.has_aes = false;
+        crypto.aes_strength = AesKeyStrength::empty();
+
+        let capabilities = Capabilities {
+            networking: self.opts.networking,
+            security: Security::empty(),
+
+            has_pfm_support: false,
+            has_policy_support: false,
+            has_firmware_protection: false,
+
+            crypto,
+        };
+
+        Ok(protocol::capabilities::DeviceCapabilitiesResponse {
+            capabilities,
+            timeouts: self.opts.timeouts,
+        })
+    }
+
+    fn handle_digests<'req>(
+        &mut self,
+        arena: &'req impl Arena,
+        req: &protocol::get_digests::GetDigestsRequest,
+    ) -> Result<protocol::get_digests::GetDigestsResponse<'req>, protocol::Error>
+    {
+        let digests_len = self
+            .opts
+            .trust_chain
+            .chain_len(req.slot)
+            .ok_or(UNSPECIFIED)?
+            .get();
+        let digests = arena
+            .alloc_slice::<[u8; hash::Algo::Sha256.bytes()]>(digests_len)
+            .map_err(|_| UNSPECIFIED)?;
+        for (i, digest) in digests.iter_mut().enumerate() {
+            let cert =
+                self.opts.trust_chain.cert(req.slot, i).ok_or(UNSPECIFIED)?;
+            self.opts
+                .hasher
+                .contiguous_hash(hash::Algo::Sha256, cert.raw(), digest)
+                .map_err(|_| UNSPECIFIED)?;
+        }
+
+        self.key_exchange = Some(req.key_exchange);
+        Ok(protocol::get_digests::GetDigestsResponse { digests })
+    }
+
+    fn handle_cert(
+        &mut self,
+        req: &protocol::get_cert::GetCertRequest,
+    ) -> Result<protocol::get_cert::GetCertResponse, protocol::Error> {
+        let cert = self
+            .opts
+            .trust_chain
+            .cert(req.slot, req.cert_number as usize)
+            .ok_or(UNSPECIFIED)?;
+
+        let start = cert.raw().len().min(req.offset as usize);
+        let end = cert
+            .raw()
+            .len()
+            .min((req.len as usize).saturating_add(start));
+        Ok(protocol::get_cert::GetCertResponse {
+            slot: req.slot,
+            cert_number: req.cert_number,
+            data: &cert.raw()[start..end],
+        })
+    }
+
+    fn handle_challenge<'req>(
+        &'req mut self,
+        arena: &'req impl Arena,
+        req: &protocol::challenge::ChallengeRequest,
+        req_buf: &[u8],
+    ) -> Result<protocol::challenge::ChallengeResponse<'req>, protocol::Error>
+    {
+        use protocol::challenge::*;
+        let signer =
+            self.opts.trust_chain.signer(req.slot).ok_or(UNSPECIFIED)?;
+        let nonce = arena.alloc::<[u8; 32]>().map_err(|_| UNSPECIFIED)?;
+        self.opts.csrng.fill(nonce).map_err(|_| UNSPECIFIED)?;
+
+        let tbs = ChallengeResponseTbs {
+            slot: req.slot,
+            slot_mask: 0, // Currently unspecified?
+            protocol_range: (0, 0),
+            nonce,
+            pmr0_components: 0,
+            pmr0: self.opts.pmr0,
+        };
+
+        let signature = arena
+            .alloc_slice::<u8>(signer.sig_bytes())
+            .map_err(|_| UNSPECIFIED)?;
+        let sig_len = tbs
+            .as_iovec_with(|[a, b, c, d]| {
+                signer.sign(&[req_buf, a, b, c, d], signature)
+            })
+            .map_err(|_| UNSPECIFIED)?;
+        let signature = &signature[..sig_len];
+
+        if let Some(KeyExchangeAlgo::Ecdh) = self.key_exchange {
+            self.opts
+                .session
+                .create_session(req.nonce, tbs.nonce)
+                .map_err(|_| UNSPECIFIED)?;
+            self.current_cert_slot = Some(tbs.slot);
+        }
+
+        Ok(ChallengeResponse { tbs, signature })
+    }
+
+    fn handle_key_xchg<'req>(
+        &mut self,
+        arena: &'req impl Arena,
+        req: &protocol::key_exchange::KeyExchangeRequest,
+    ) -> Result<
+        protocol::key_exchange::KeyExchangeResponse<'req>,
+        protocol::Error,
+    > {
+        use protocol::key_exchange::*;
+        match req {
+            KeyExchangeRequest::SessionKey {
+                hmac_algorithm: _,
+                pk_req,
+            } => {
+                let signer = self
+                    .opts
+                    .trust_chain
+                    .signer(self.current_cert_slot.ok_or(UNSPECIFIED)?)
+                    .ok_or(UNSPECIFIED)?;
+
+                let pk_resp = arena
+                    .alloc_slice(self.opts.session.ephemeral_bytes())
+                    .map_err(|_| UNSPECIFIED)?;
+                let key_len = self
+                    .opts
+                    .session
+                    .begin_ecdh(pk_resp)
+                    .map_err(|_| UNSPECIFIED)?;
+                let pk_resp = &pk_resp[..key_len];
+                self.opts
+                    .session
+                    .finish_ecdh(pk_req)
+                    .map_err(|_| UNSPECIFIED)?;
+
+                let signature = arena
+                    .alloc_slice(signer.sig_bytes())
+                    .map_err(|_| UNSPECIFIED)?;
+                signer
+                    .sign(&[pk_req, pk_resp], signature)
+                    .map_err(|_| UNSPECIFIED)?;
+
+                Ok(KeyExchangeResponse::SessionKey {
+                    pk_resp,
+                    signature,
+                    alias_cert_hmac: &[
+                      // Pending require of crypto::sha256.
+                    ],
+                })
+            }
+            _ => Err(UNSPECIFIED),
+        }
     }
 }
 
